47e91b264d24106a10b4cde1d4e68c1c43bb80cb
albertoaguadosegovia <alberto.aguadosegovia@cieep.com>
2023-09-18 09:31:56 +0200

    Initial commit

---
+++ app/src/main/java/alberto/aguado/ejemplo2guardarenlista/MainActivity.java
@@ -0,0 +1,14 @@
+package alberto.aguado.ejemplo2guardarenlista;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
